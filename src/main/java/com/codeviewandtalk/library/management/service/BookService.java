dc546bd25fbe8a0ae33a4c57c0a1c4e639c0f064
codeviewandtalk <codeviewandtalkbypiyush@gmail.com>
2025-06-04 22:50:25 +0530

    logger added in book service

--- src/main/java/com/codeviewandtalk/library/management/service/BookService.java
+++ src/main/java/com/codeviewandtalk/library/management/service/BookService.java
@@ -16,6 +16,7 @@ public class BookService {
     }
 
     public List<Book> getBooksByAuthorName(String authorName) {
+        System.out.println("Author name: " + authorName);
         return bookRepository.findByAuthor_Name(authorName);
     }
 }
