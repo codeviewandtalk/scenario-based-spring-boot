9018bb85c7b62499db90e5305d70b8f5b995481b
codeviewandtalk <codeviewandtalkbypiyush@gmail.com>
2025-07-19 18:43:49 +0530

    spring profiles implementations

--- src/main/java/com/codeviewandtalk/library/management/controller/HealthController.java
+++ src/main/java/com/codeviewandtalk/library/management/controller/HealthController.java
@@ -1,13 +1,18 @@
 package com.codeviewandtalk.library.management.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
+
+    @Value("${greet.message}")
+    String message;
 
     @GetMapping("/health")
     public String getHealthStatus() {
-        return "Library Management System is up and running!";
+        return message;
     }
 }
